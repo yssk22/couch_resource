7bfb092ae646396bab2b37f7c6e275a5b3deb00e
Yohei Sasaki <yssk22@gmail.com>
2009-03-10 03:07:41 +0900

    Initial commit to couch_resource.

---
+++ test/test_helper.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'test/unit'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'couch_resource'
+
+class Test::Unit::TestCase
+end
